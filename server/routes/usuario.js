ab769b0dcb58da7ae0d76131bfb3e73aba42284b
Pablo <pablobonnaluc@gmail.com>
2019-04-15 18:09:59 -0300

    Login normal seccion 10

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -5,9 +5,11 @@ const _= require('underscore');
 
 const Usuario = require('../models/usuario');
 
+const { verificaToken,verificaAdmin_Role } = require('../middlewares/autenticacion');
+
 const  app = express()
 
-app.get('/usuario', function (req, res) {
+app.get('/usuario', verificaToken ,  function (req, res) {
 
         let desde = req.query.desde || 0;
         desde = Number(desde);
@@ -44,7 +46,7 @@ app.get('/usuario', function (req, res) {
 
   })
   
-  app.post('/usuario', function (req, res) {
+  app.post('/usuario', [verificaToken,verificaAdmin_Role] , function (req, res) {
   
       let body = req.body;
 
@@ -75,7 +77,7 @@ app.get('/usuario', function (req, res) {
       
   })
   
-  app.put('/usuario/:id', function (req, res) {
+  app.put('/usuario/:id', [verificaToken,verificaAdmin_Role] ,  (req, res) => {
   
       let id = req.params.id;
       let body = _.pick( req.body , ['nombre','email','img','role','estado']);
@@ -100,7 +102,7 @@ app.get('/usuario', function (req, res) {
     
   })
   
-  app.delete('/usuario/:id', function (req, res) {
+  app.delete('/usuario/:id', [verificaToken,verificaAdmin_Role] , function (req, res) {
      
         let id = req.params.id;
 
